a56ccd425d8c8ce21cb050b7ff8787691bdab6a3
Karan Pal Singh <kdsgyt@gmail.com>
2020-12-28 06:41:55 -0500

    Firebase is working

--- src/assets/functions.tsx
+++ src/assets/functions.tsx
@@ -1,16 +1,17 @@
-import fireAuth from '../Util/Firebase';
+import fireAuth, { googleSigninProvider } from '../Util/Firebase';
 
 /**
  * Login with Email and password onto account
  * @param email used as username
  * @param password password to login
+ * @param setUserState used to set the global user state
  */
 export function loginWithEmailAndPassword(email: string, password: string, setUserState: any) {
   fireAuth.signInWithEmailAndPassword(email, password)
     .then(data => data.user)
     .then((userData) => {
       // Save the user information in the global state
-      setUserState(userData); 
+      setUserState(userData);
       // ...
     })
     .catch((error) => {
@@ -18,4 +19,23 @@ export function loginWithEmailAndPassword(email: string, password: string, setUs
       var errorMessage = error.message;
       console.error(error);
     });
+}
+
+export function loginWithGoogle() {
+  fireAuth.signInWithPopup(googleSigninProvider)
+    .then(result => {
+      console.log(result)
+    })
+    .catch(function (error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      console.error(error)
+      // ...
+    });
+  return;
 }
--- src/components/GoogleLogin/GoogleLogin.tsx
+++ src/components/GoogleLogin/GoogleLogin.tsx
@@ -1,12 +1,15 @@
 import { Button } from "@material-ui/core";
+import { loginWithGoogle } from "../../assets/functions";
 import './GoogleLogin.scss';
+
+// import * as signInWithGoogleGraphic from '../../assets/img/google-login.png'
+
 const GoogleLogin = () =>{
     return (
         <Button 
             id="google-login-button"
+            onClick={loginWithGoogle}
         >
-            <span id="logo"></span>
-            SignIn With Google
         </Button>
     )
 }
